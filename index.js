ac188c8e435cb91b07502ecd9e820c3fe21944b5
unknown <abubakar471>
2023-04-25 17:24:19 +0600

    changes have been made for deploying

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ mongoose.connect(process.env.DATABASE)
 // middlewares
 app.use(cors());
 app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // routes
 // readdirSync('./routes').map(r => app.use('/api', require(`./routes/${r}`)));
